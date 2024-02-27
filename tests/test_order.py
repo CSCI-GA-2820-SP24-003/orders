84fd7a3bc01e1e6a990ca96df6abd30c7cea080a
Ihwa Hussain <fh828@nyu.edu>
2024-02-27 03:20:28 +0000

    Added test file template for order, see #14

---
+++ tests/test_order.py
@@ -0,0 +1,66 @@
+######################################################################
+# Copyright 2016, 2024 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Test cases for Order Model
+"""
+
+import logging
+import os
+from unittest import TestCase
+from unittest.mock import patch
+from wsgi import app
+from service.models import Order, Item, DataValidationError, db
+from tests.factories import OrderFactory, ItemFactory
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/postgres"
+)
+
+
+######################################################################
+#        O R D E R   M O D E L   T E S T   C A S E S
+######################################################################
+class TestOrder(TestCase):
+    """Order Model Test Cases"""
+
+    @classmethod
+    def setUpClass(cls):
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        app.app_context().push()
+
+    @classmethod
+    def tearDownClass(cls):
+        """This runs once after the entire test suite"""
+        db.session.close()
+
+    def setUp(self):
+        """This runs before each test"""
+        db.session.query(Order).delete()  # clean up the last tests
+        db.session.query(Item).delete()  # clean up the last tests
+        db.session.commit()
+
+    def tearDown(self):
+        """This runs after each test"""
+        db.session.remove()
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################

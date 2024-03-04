41a0dd46ce9c941febf3e8da9711b01e7555f2f4
anishabhatnagar <anishabhatnagar2511@gmail.com>
2024-03-04 01:38:44 +0000

    added create orders api

--- service/routes.py
+++ service/routes.py
@@ -22,7 +22,7 @@ This service implements a REST API that allows you to manage Orders for a financ
 
 from flask import jsonify
 
-# , request, url_for, abort
+from flask import request, url_for, abort
 from flask import current_app as app  # Import Flask application
 
 from service.models import Order
@@ -51,7 +51,6 @@ def index():
 ######################################################################
 
 
-# Todo: Place your REST API code here ...
 ######################################################################
 # LIST ALL ORDERS
 ######################################################################
@@ -67,3 +66,52 @@ def list_orders():
     results = [order.serialize() for order in orders]
 
     return jsonify(results), status.HTTP_200_OK
+
+
+
+######################################################################
+# CREATE A NEW ACCOUNT
+######################################################################
+@app.route("/orders", methods=["POST"])
+def create_orders():
+    """
+    Creates an Order
+    This endpoint will create an Order based the data in the body that is posted
+    """
+    app.logger.info("Request to create an Order")
+    check_content_type("application/json")
+
+    # Create the order
+    order = Order()
+    order.deserialize(request.get_json())
+    order.create()
+
+    # Create a message to return
+    message = order.serialize()
+    location_url = url_for("get_orders", order_id=order.id, _external=True)
+
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+# Todo: Place your REST API code here ...
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, f"Content-Type must be {content_type}"
+    )
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,6 +14,7 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
 
+BASE_URL = "/orders"
 
 ######################################################################
 #  T E S T   C A S E S
@@ -46,6 +47,27 @@ class TestYourResourceService(TestCase):
     def tearDown(self):
         """This runs after each test"""
         db.session.remove()
+
+    ######################################################################
+    #  H E L P E R   M E T H O D S
+    ######################################################################
+
+    def _create_orders(self, count):
+        """Factory method to create orders in bulk"""
+        orders = []
+        for _ in range(count):
+            order = OrderFactory()
+            resp = self.client.post(BASE_URL, json=order.serialize())
+            self.assertEqual(
+                resp.status_code,
+                status.HTTP_201_CREATED,
+                "Could not create test Order",
+            )
+            new_order = resp.get_json()
+            order.id = new_order["id"]
+            orders.append(order)
+        return orders
+
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
@@ -88,4 +110,75 @@ class TestYourResourceService(TestCase):
             self.assertEqual(order_data["customer_id"], order.customer_id)
             # Add more assertions here to check other fields if necessary
 
+    def test_create_order(self):
+        """It should Create a new Order"""
+        order = OrderFactory()
+        resp = self.client.post(
+            BASE_URL, json=order.serialize(), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = resp.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_order = resp.get_json()
+        self.assertEqual(new_order["id"], order.id, "id does not match")
+        self.assertEqual(
+            new_order["customer_id"], order.customer_id, "customer_id does not match"
+        )
+        self.assertEqual(new_order["order_date"], order.order_date, "order_date does not match")
+        self.assertEqual(
+            new_order["status"], order.status.name, "status does not match"
+        )
+        self.assertEqual(
+            new_order["total_amount"], order.total_amount, "total_amount does not match"
+        )
+        self.assertEqual(
+            new_order["payment_method"], order.payment_method, "payment_method does not match"
+        )
+        self.assertEqual(
+            new_order["shipping_cost"], order.shipping_cost, "shipping_cost does not match"
+        )
+        self.assertEqual(
+            new_order["expected_date"],
+            str(order.expected_date),
+            "expected_date does not match",
+        )
+        self.assertEqual(
+            new_order["order_notes"], order.order_notes, "order_notes does not match"
+        )
+        
+
+        # Check that the location header was correct by getting it
+        resp = self.client.get(location, content_type="application/json")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_order = resp.get_json()
+        self.assertEqual(new_order["id"], order.id, "id does not match")
+        self.assertEqual(
+            new_order["customer_id"], order.customer_id, "customer_id does not match"
+        )
+        self.assertEqual(new_order["order_date"], str(order.order_date), "order_date does not match")
+        self.assertEqual(
+            new_order["status"], order.status, "status does not match"
+        )
+        self.assertEqual(
+            new_order["total_amount"], order.total_amount, "total_amount does not match"
+        )
+        self.assertEqual(
+            new_order["payment_method"], order.payment_method, "payment_method does not match"
+        )
+        self.assertEqual(
+            new_order["shipping_cost"], order.shipping_cost, "shipping_cost does not match"
+        )
+        self.assertEqual(
+            new_order["expected_date"],
+            str(order.expected_date),
+            "expected_date does not match",
+        )
+        self.assertEqual(
+            new_order["order_notes"], order.order_notes, "order_notes does not match"
+        )
+    
     # Todo: Add your test cases here...
